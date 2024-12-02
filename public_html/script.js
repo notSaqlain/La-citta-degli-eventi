6f7570ec9c45715759df088e8272a85afeee0278
notSaqlain <iamnotsaqlain@gmail.com>
2024-12-02 19:55:56 +0100

    Add script.js

---
+++ public_html/script.js
@@ -0,0 +1,10 @@
+// File script.js - La città degli eventi
+
+// Inizia qui il tuo lavoro! Usa gli eventi per animare la città degli eventi.
+
+// Esempio di struttura iniziale
+document.addEventListener('DOMContentLoaded', () => {
+    console.log('Benvenuto nella città degli eventi!');
+    
+    // Aggiungi qui i tuoi eventi
+});
